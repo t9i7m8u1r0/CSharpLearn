9840b8b8bfbbcfc1d919c7024121b63c4c8a4d2a
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-13 23:39:47 +0700

    Добавьте файлы проекта.

---
+++ CSharpLearn/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+        var color = Console.ReadLine();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+
+            default:
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+        }
+    }
+}
