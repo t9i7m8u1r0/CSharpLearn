b3876200789a7cd422801db6be0bc35e0811eeff
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-15 01:59:50 +0700

    4.2.11

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,39 +3,14 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-        var color = Console.ReadLine();
-
-        switch (color)
+        int t = 0;
+        do
         {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is red!");
-                break;
-
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-                break;
-
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-                break;
-
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.WriteLine("Your color is yellow!");
-                break;
+            Console.WriteLine(t);
+            Console.WriteLine("Write your favorite color");
+            switch (Console.ReadLine()) { }
+            t++;
         }
+        while (t < 3);
     }
 }
