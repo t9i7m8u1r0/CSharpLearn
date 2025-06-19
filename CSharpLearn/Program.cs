fa4cd022e29a3f0449c402831017ab4479a74244
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-19 14:43:33 +0700

    4.3.10

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -4,9 +4,6 @@ class MainClass
     public static void Main(string[] args)
     {
         int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
-        foreach (int i in array)
-        {
-            Console.WriteLine(i);
-        }
+        Console.WriteLine(array.Length);
     }
 }
