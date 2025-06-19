54aa9512f5f9e634c38bb5d210de5877f6a9095b
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-20 00:48:53 +0700

    4.3.13

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -5,24 +5,13 @@ class MainClass
     {
         var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-        int temp;
+        int sum = 0;
 
         for (int i = 0; i < arr.Length; i++)
         {
-            for (int j = i + 1; j < arr.Length; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+            sum += arr[i];
         }
 
-        foreach (var item in arr)
-        {
-            Console.Write(item);
-        }
+        Console.WriteLine(sum);
     }
 }
