f77f669d6f06322a174a27ac311c51b1d3812397
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-19 19:28:07 +0700

    4.3.11

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,7 +3,15 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
-        Console.WriteLine(array.Length);
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+        for (int i = 0; i <= array.GetUpperBound(1); i++)
+        {
+            for (int j = 0; j <= array.GetUpperBound(0); j++)
+            {
+                Console.Write($"{array[j, i]} ");
+            }
+            Console.WriteLine();
+        }
     }
 }
