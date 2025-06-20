97de7c5af8fac0fdcb1090b900a616aa1ec0edf6
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-20 15:09:05 +0700

    4.3.14

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,15 +3,18 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int[] arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+        int[][] array = new int[3][];
 
-        int sum = 0;
+        array[0] = new int[2] { 1, 2 };
+        array[1] = new int[3] { 1, 2, 3 };
+        array[2] = new int[5] { 1, 2, 3, 4, 5 };
 
-        for (int i = 0;  i < arr.Length; i++)
+        foreach (int[] i in array)
         {
-            sum += arr[i];
+            foreach (int j in i)
+            {
+                Console.Write($"{j} ");
+            }
         }
-
-        Console.WriteLine(sum);
     }
 }
