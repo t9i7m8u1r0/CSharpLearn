e89d343dd13374005459486bd9db331e7dcf9d45
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-20 11:56:02 +0700

    4.3.13

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,11 +3,11 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+        int[] arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
         int sum = 0;
 
-        for (int i = 0; i < arr.Length; i++)
+        for (int i = 0;  i < arr.Length; i++)
         {
             sum += arr[i];
         }
