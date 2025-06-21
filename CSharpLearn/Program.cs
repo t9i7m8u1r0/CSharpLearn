c8eeb4f8d9f51a20e38dbadfe5ced9a53621c3b9
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 01:10:51 +0700

    4.4.2

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,21 +3,9 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+        (string name, int age) anketa = (name: Console.ReadLine(),
+            age: Convert.ToInt32(Console.ReadLine()));
 
-        int count = 0;
-
-        for (int i = 0; i <= arr.GetUpperBound(0); i++)
-        {
-            for (int j = 0; j <= arr.GetUpperBound(1); j++)
-            {
-                if (arr[i,j] > 0)
-                {
-                    count++;
-                }
-            }
-        }
-
-        Console.WriteLine(count);
+        Console.WriteLine(anketa);
     }
 }
