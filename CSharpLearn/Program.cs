340f648f48406b02d36a7e1e0cb6b72f4639988e
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 01:31:49 +0700

    4.4.3

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,9 +3,16 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string name, int age) anketa = (name: Console.ReadLine(),
-            age: Convert.ToInt32(Console.ReadLine()));
+        var (name, age) = ("Евгения", 27);
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
 
-        Console.WriteLine(anketa);
+        Console.Write("Введите имя: ");
+        name = Console.ReadLine();
+
+        Console.Write("Введите возраст с цифрами: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"Ваше имя: {name}\nВаш возраст: {age}");
     }
 }
