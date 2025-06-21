42f01708b4a63dcb321340ca1369d427f3d85634
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 02:04:06 +0700

    4.4.5

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,16 +3,18 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        var (name, age) = ("Евгения", 27);
-        Console.WriteLine("Ваше имя: {0}", name);
-        Console.WriteLine("Ваш возраст: {0}", age);
+        (string Name, string Type, double Age, int NameCount) Pet;
 
-        Console.Write("Введите имя: ");
-        name = Console.ReadLine();
+        Console.Write("Введите имя питомца: ");
+        Pet.Name = Console.ReadLine();
 
-        Console.Write("Введите возраст с цифрами: ");
-        age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите тип питомца: ");
+        Pet.Type = Console.ReadLine();
 
-        Console.Write($"Ваше имя: {name}\nВаш возраст: {age}");
+        Console.Write("Введите возраст питомца: ");
+        Pet.Age = (Convert.ToDouble(Console.ReadLine()));
+
+        Pet.NameCount = Pet.Name.Length;
+        Console.Write($"Длина имени питомца: {Pet.NameCount}");
     }
 }
