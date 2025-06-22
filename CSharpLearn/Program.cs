1b27f21069817e4864698e891b8862ad9f3a76ac
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 22:35:05 +0700

    4.5.6

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -11,29 +11,35 @@ class MainClass
             double age,
             string[] favColors) User;
 
-        Console.Write("Введите имя: ");
-        User.name = Console.ReadLine();
-
-        Console.Write("Введите фамилию: ");
-        User.surname = Console.ReadLine();
-
-        Console.Write("Введите логин: ");
-        User.login = Console.ReadLine();
-
-        User.loginLength = User.login.Length;
-
-        Console.Write("Есть ли у вас животные? Введите \"Да\" или \"Нет\": ");
-        string inputHasPet = Console.ReadLine();
-        User.hasPet = inputHasPet == "Да" ? true : false;
-
-        Console.Write("Введите возраст пользователя: ");
-        User.age = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Введите три любимых цвета пользователя: ");
         User.favColors = new string[3];
-        for (int i = 0; i < User.favColors.Length; i++)
+
+        for (int j = 0; j < 3; j++)
         {
-            User.favColors[i] = Console.ReadLine();
+
+            Console.Write("Введите имя: ");
+            User.name = Console.ReadLine();
+
+            Console.Write("Введите фамилию: ");
+            User.surname = Console.ReadLine();
+
+            Console.Write("Введите логин: ");
+            User.login = Console.ReadLine();
+            User.loginLength = User.login.Length;
+
+            Console.Write("Есть ли у вас животные? Введите \"Да\" или \"Нет\": ");
+            string inputHasPet = Console.ReadLine();
+            User.hasPet = inputHasPet == "Да" ? true : false;
+
+            Console.Write("Введите возраст пользователя: ");
+            User.age = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите три любимых цвета пользователя:");
+
+            for (int i = 0; i < User.favColors.Length; i++)
+            {
+                Console.Write($"{i+1}) ");
+                User.favColors[i] = Console.ReadLine();
+            }
         }
     }
 }
