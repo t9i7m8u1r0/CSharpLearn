081e2942f7a1e33fb0634f9b6e6fe1f69ef1fad1
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 21:30:23 +0700

    4.5.2

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,12 +3,21 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string username,
+        (string name,
             string surname,
             string login,
             int loginLength,
             bool hasPet,
-            int age,
+            double age,
             string[] favColors) User;
+
+        Console.Write("Введите имя: ");
+        User.name = Console.ReadLine();
+
+        Console.Write("Введите фамилию: ");
+        User.surname = Console.ReadLine();
+
+        Console.Write("Введите логин");
+        User.login = Console.ReadLine();
     }
 }
