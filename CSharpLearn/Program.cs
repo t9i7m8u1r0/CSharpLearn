cf89b78bc5776c0572ea0ed4972fc10e50ffaa75
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 21:03:40 +0700

    4.5.1

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,18 +3,12 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string Name, string Type, double Age, int NameCount) Pet;
-
-        Console.Write("Введите имя питомца: ");
-        Pet.Name = Console.ReadLine();
-
-        Console.Write("Введите тип питомца: ");
-        Pet.Type = Console.ReadLine();
-
-        Console.Write("Введите возраст питомца: ");
-        Pet.Age = (Convert.ToDouble(Console.ReadLine()));
-
-        Pet.NameCount = Pet.Name.Length;
-        Console.Write($"Длина имени питомца: {Pet.NameCount}");
+        (string username,
+            string surname,
+            string login,
+            int loginLength,
+            bool hasPet,
+            int age,
+            string[] favColors) User;
     }
 }
