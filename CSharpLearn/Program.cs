ff64b5e0060fd9c30530ed1b80d80b5e5fcb0628
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 22:04:09 +0700

    4.5.4

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -17,9 +17,14 @@ class MainClass
         Console.Write("Введите фамилию: ");
         User.surname = Console.ReadLine();
 
-        Console.Write("Введите логин");
+        Console.Write("Введите логин: ");
         User.login = Console.ReadLine();
 
         User.loginLength = User.login.Length;
+
+        Console.Write("Есть ли у вас животные? Введите \"Да\" или \"Нет\": ");
+
+        string inputHasPet = Console.ReadLine();
+        User.hasPet = inputHasPet == "Да" ? true : false;
     }
 }
