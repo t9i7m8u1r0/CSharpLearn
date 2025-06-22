a6190af6695352c8ede4419c64e0f0fc4e8d1052
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-23 00:48:10 +0700

    5.0.1

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,43 +3,14 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string name,
-            string surname,
-            string login,
-            int loginLength,
-            bool hasPet,
-            double age,
-            string[] favColors) User;
+        (string Name, string[] Dishes) User;
+        User.Dishes = new string[5];
 
-        User.favColors = new string[3];
+        User.Name = Console.ReadLine();
 
-        for (int j = 0; j < 3; j++)
+        for (int i = 0;  i < User.Dishes.Length; i++)
         {
-
-            Console.Write("Введите имя: ");
-            User.name = Console.ReadLine();
-
-            Console.Write("Введите фамилию: ");
-            User.surname = Console.ReadLine();
-
-            Console.Write("Введите логин: ");
-            User.login = Console.ReadLine();
-            User.loginLength = User.login.Length;
-
-            Console.Write("Есть ли у вас животные? Введите \"Да\" или \"Нет\": ");
-            string inputHasPet = Console.ReadLine();
-            User.hasPet = inputHasPet == "Да" ? true : false;
-
-            Console.Write("Введите возраст пользователя: ");
-            User.age = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите три любимых цвета пользователя:");
-
-            for (int i = 0; i < User.favColors.Length; i++)
-            {
-                Console.Write($"{i+1}) ");
-                User.favColors[i] = Console.ReadLine();
-            }
+            User.Dishes[i] = Console.ReadLine();
         }
     }
 }
