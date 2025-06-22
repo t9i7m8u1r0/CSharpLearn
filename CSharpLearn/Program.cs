8b03359cafaabc4b8dcae85ae7cc12884b136038
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-22 22:19:49 +0700

    4.5.5

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -23,8 +23,17 @@ class MainClass
         User.loginLength = User.login.Length;
 
         Console.Write("Есть ли у вас животные? Введите \"Да\" или \"Нет\": ");
-
         string inputHasPet = Console.ReadLine();
         User.hasPet = inputHasPet == "Да" ? true : false;
+
+        Console.Write("Введите возраст пользователя: ");
+        User.age = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите три любимых цвета пользователя: ");
+        User.favColors = new string[3];
+        for (int i = 0; i < User.favColors.Length; i++)
+        {
+            User.favColors[i] = Console.ReadLine();
+        }
     }
 }
