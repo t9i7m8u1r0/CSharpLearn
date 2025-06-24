6007ab25caf388b3028d9cb5cb2f5b49cf6a7c47
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-25 01:45:09 +0700

    5.3.13

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -5,13 +5,60 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int num = 5;
-        int[] array = new int[num];
-        GetArrayFromConsole(ref num);
-        Console.WriteLine(num);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        SortArray(arr, out arr, out arr);
     }
-    public static void GetArrayFromConsole(ref int num)
+    public static void SortArray(in int[] array, out int[] sorteddesc, out int[] sortedasc)
     {
-        num = 6;
+        sortedasc = SortArrayAsc(array);
+        sorteddesc = SortArrayDesc(array);
+    }
+    public static int[] SortArrayDesc(int[] sorteddesc)
+    {
+        int temp;
+        for (int i = 0; i < sorteddesc.Length; i++)
+        {
+            for (int j = i + 1; j < sorteddesc.Length; j++)
+            {
+                if (sorteddesc[i] < sorteddesc[j])
+                {
+                    temp = sorteddesc[i];
+                    sorteddesc[i] = sorteddesc[j];
+                    sorteddesc[j] = temp;
+                }
+            }
+        }
+        foreach (int i in sorteddesc)
+        {
+            Console.WriteLine(i);
+        }
+        return sorteddesc;
+    }
+    public static int[] SortArrayAsc(int[] sortedasc)
+    {
+        int temp;
+        for (int i = 0; i < sortedasc.Length; i++)
+        {
+            for (int j = i + 1; j < sortedasc.Length; j++)
+            {
+                if (sortedasc[i] > sortedasc[j])
+                {
+                    temp = sortedasc[i];
+                    sortedasc[i] = sortedasc[j];
+                    sortedasc[j] = temp;
+                }
+            }
+        }
+        foreach (int i in sortedasc)
+        {
+            Console.WriteLine(i);
+        }
+        return sortedasc;
     }
 }
