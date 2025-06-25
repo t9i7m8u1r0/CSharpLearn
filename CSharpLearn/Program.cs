9986e701a624f93ad5468bf8679c7b6f991ae6af
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-06-25 12:04:49 +0700

    5.3.13

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -6,59 +6,78 @@ class MainClass
     public static void Main(string[] args)
     {
         int[] arr = new int[5];
-
+        Console.WriteLine("Введите пять чисел");
         for (int i = 0; i < arr.Length; i++)
         {
             arr[i] = int.Parse(Console.ReadLine());
         }
 
-        SortArray(arr, out arr, out arr);
+        int[] sortedask;
+        int[] sorteddesk;
+
+        SortArray(arr, out sorteddesk, out sortedask);
+
+        Console.WriteLine("По возрастанию:");
+        foreach (int i in sortedask)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine("\nПо убыванию:");
+        foreach (int i in sorteddesk)
+        {
+            Console.Write(i + " ");
+        }
     }
     public static void SortArray(in int[] array, out int[] sorteddesc, out int[] sortedasc)
     {
         sortedasc = SortArrayAsc(array);
         sorteddesc = SortArrayDesc(array);
     }
-    public static int[] SortArrayDesc(int[] sorteddesc)
+    public static int[] SortArrayDesc(int[] array)
     {
-        int temp;
-        for (int i = 0; i < sorteddesc.Length; i++)
+        int[] sorted = new int[array.Length];
+        for (int i = 0; i < array.Length;i++)
         {
-            for (int j = i + 1; j < sorteddesc.Length; j++)
+            sorted[i] = array[i];
+        }
+
+        int temp;
+        for (int i = 0; i < sorted.Length; i++)
+        {
+            for (int j = i + 1; j < sorted.Length; j++)
             {
-                if (sorteddesc[i] < sorteddesc[j])
+                if (sorted[i] < sorted[j])
                 {
-                    temp = sorteddesc[i];
-                    sorteddesc[i] = sorteddesc[j];
-                    sorteddesc[j] = temp;
+                    temp = sorted[i];
+                    sorted[i] = sorted[j];
+                    sorted[j] = temp;
                 }
             }
         }
-        foreach (int i in sorteddesc)
-        {
-            Console.WriteLine(i);
-        }
-        return sorteddesc;
+        return sorted;
     }
-    public static int[] SortArrayAsc(int[] sortedasc)
+    public static int[] SortArrayAsc(int[] array)
     {
-        int temp;
-        for (int i = 0; i < sortedasc.Length; i++)
+        int[] sorted = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
         {
-            for (int j = i + 1; j < sortedasc.Length; j++)
+            sorted[i] = array[i];
+        }
+
+        int temp;
+        for (int i = 0; i < sorted.Length; i++)
+        {
+            for (int j = i + 1; j < sorted.Length; j++)
             {
-                if (sortedasc[i] > sortedasc[j])
+                if (sorted[i] > sorted[j])
                 {
-                    temp = sortedasc[i];
-                    sortedasc[i] = sortedasc[j];
-                    sortedasc[j] = temp;
+                    temp = sorted[i];
+                    sorted[i] = sorted[j];
+                    sorted[j] = temp;
                 }
             }
         }
-        foreach (int i in sortedasc)
-        {
-            Console.WriteLine(i);
-        }
-        return sortedasc;
+        return sorted;
     }
 }
