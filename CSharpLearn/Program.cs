44f2a401e2d13a1a4e3d03c57b99e4a9f86ade52
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-07-06 02:05:34 +0700

    5.6.1 (HW-03)

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -3,24 +3,78 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(PowerUp(3, 4));
+        ShowUserInfo(GetUserInfo());
     }
-    private static int PowerUp(int N, byte pow = 1)
+    public static (string, string, int, string[], string[]) GetUserInfo()
     {
-        if (pow == 0)
+        Console.Write("Введите имя: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Введите фамилию: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Введите свой возраст: ");
+        int age = CheckNumber();
+
+        string[] petName = new string[0];
+
+        Console.Write("Есть ли у вас питомцы (Да/Нет)? ");
+        string hasPet = Console.ReadLine();
+
+        if (hasPet == "Да")
         {
-            return 1;
+            Console.Write("Сколько? ");
+            petName = GetNames(CheckNumber());
         }
-        else
+
+        Console.Write("Сколько у вас любимых цветов? ");
+        string[] favColor = GetNames(CheckNumber());
+
+        return (firstName, lastName, age, petName, favColor);
+    }
+    public static int CheckNumber()
+    {
+        int number;
+        string input;
+
+        do
         {
-            if (pow == 1)
+            input = Console.ReadLine();
+        }
+        while (!int.TryParse(input, out number) || number <= 0);
+
+        return number;
+    }
+    public static string[] GetNames(int count)
+    {
+        string[] names = new string[count];
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.Write($"Введите номер {i + 1}: ");
+            names[i] = Console.ReadLine();
+        }
+
+        return names;
+    }
+    public static void ShowUserInfo((string, string, int, string[], string[]) info)
+    {
+        Console.WriteLine($"\n{info.Item2} {info.Item1}, возраст: {info.Item3}");
+
+        if (info.Item4.Length > 0)
+        {
+            Console.WriteLine("Ваши питомцы:");
+
+            for (int i = 0; i < info.Item4.Length; i++)
             {
-                return N;
+                Console.WriteLine($"{i + 1}) {info.Item4[i]}");
             }
-            else
-            {
-                return N * PowerUp(N, --pow);
-            }
+        }
+
+        Console.WriteLine("Ваши любимые цвета:");
+        for (int i = 0; i < info.Item5.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}) {info.Item5[i]}");
         }
     }
 }
