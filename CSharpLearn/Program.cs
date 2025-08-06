2c55bae5b376372848275ae32c3eabda80735c7a
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-08-07 01:02:15 +0700

    6.5.2

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -6,19 +6,59 @@ class MainClass
         
     }
 }
-class Bus
-{
-    public int? Load;
 
-    public void PrintStatus()
+class GeometricFigure
+{
+    class Triangle
     {
-        if (Load.HasValue && Load > 0)
+        public double sideA;
+        public double sideB;
+        public double sideC;
+
+        public static double Area(double baseSide, double height)
         {
-            Console.WriteLine($"В автобусе {Load.Value} человек");
+            double triangleArea = 0.5 * baseSide * height;
+            return triangleArea;
         }
-        else
+
+        public static double Perimeter(double firstSide, double secondSide, double thirdSide)
         {
-            Console.WriteLine("Пусто");
+            double trianglePerimeter = firstSide + secondSide + thirdSide;
+            return trianglePerimeter;
+        }
+    }
+
+    class Circle
+    {
+        public double radius;
+
+        public static double Area(double radiusCircle)
+        {
+            double circleArea = Math.PI * radiusCircle * radiusCircle;
+            return circleArea;
+        }
+
+        public static double Circumference(double radiusCircle)
+        {
+            double circumference = 2 * Math.PI * radiusCircle;
+            return circumference;
+        }
+    }
+
+    class Square
+    {
+        public double side;
+
+        public static double Area(double side)
+        {
+            double squareArea = side * side;
+            return squareArea;
+        }
+
+        public static double Perimeter(double side)
+        {
+            double squareArea = side * 4;
+            return squareArea;
         }
     }
 }
