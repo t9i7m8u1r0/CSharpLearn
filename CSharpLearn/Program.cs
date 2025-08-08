dff2383f5a2aba8b3438dd3f5ec5034276199e30
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-08-08 14:54:07 +0700

    6.6.1

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -6,59 +6,14 @@ class MainClass
         
     }
 }
-
-class GeometricFigure
+class TrafficLight
 {
-    class Triangle
+    private static void ChangeColor(string color)
     {
-        public double sideA;
-        public double sideB;
-        public double sideC;
 
-        public static double Area(double baseSide, double height)
-        {
-            double triangleArea = 0.5 * baseSide * height;
-            return triangleArea;
-        }
-
-        public static double Perimeter(double firstSide, double secondSide, double thirdSide)
-        {
-            double trianglePerimeter = firstSide + secondSide + thirdSide;
-            return trianglePerimeter;
-        }
     }
-
-    class Circle
+    public static string GetColor()
     {
-        public double radius;
-
-        public static double Area(double radiusCircle)
-        {
-            double circleArea = Math.PI * radiusCircle * radiusCircle;
-            return circleArea;
-        }
-
-        public static double Circumference(double radiusCircle)
-        {
-            double circumference = 2 * Math.PI * radiusCircle;
-            return circumference;
-        }
-    }
-
-    class Square
-    {
-        public double side;
-
-        public static double Area(double side)
-        {
-            double squareArea = side * side;
-            return squareArea;
-        }
-
-        public static double Perimeter(double side)
-        {
-            double squareArea = side * 4;
-            return squareArea;
-        }
+        
     }
 }
