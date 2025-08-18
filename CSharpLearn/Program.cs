7d0b35f7c8fb666f7cca8f68165cada9ce2477f4
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-08-19 00:22:49 +0700

    7.1.5

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -6,19 +6,11 @@
     }
 }
 
-class Employee
-{
-    public string Name;
-    public int Age;
-    public int Salary;
-}
-
-class ProjectManager : Employee
-{
-    public string ProjectName;
-}
-
-class Developer : Employee
-{
-    public string ProgrammingLanguage;
-}
+class Food { }
+class Fruit : Food { }
+class Vegetable : Food { }
+class Apple : Fruit { }
+class Banana : Fruit { }
+class Pear : Fruit { }
+class Potato : Vegetable { }
+class Carrot : Vegetable { }
