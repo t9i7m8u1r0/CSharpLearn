9fcf4ec0f9ad74d82e6f862167ef45e9c5d64c42
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-09-29 02:33:29 +0700

    7.5.3

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -6,15 +6,25 @@ namespace CSharpLearn
     {
         public static void Main(string[] args)
         {
-            //7.5.2
+            //7.5.3
+
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
         }
     }
 
-    class Obj
+    class Helper
     {
-        static int MaxValue = 2000;
-
-        string Name;
-        string Description;
+        public static void Swap(ref int num1, ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
     }
 }
