5e8ca9d2380262307c51fa8fd43bd42f3d160d41
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-10-15 19:50:14 +0700

    7.6.2

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -6,37 +6,16 @@ namespace CSharpLearn
     {
         public static void Main(string[] args)
         {
-            //7.5.9
-
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
-
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
+            
         }
     }
 
-    static class IntExtensions
+    class Car<T>
     {
-        public static int GetNegative(this int number)
-        {
-            if (number <= 0)
-                return number;
-            else
-                return -number;
-        }
-
-        public static int GetPositive(this int number)
-        {
-            if (number >= 0)
-                return number;
-            else
-                return -number;
-        }
+        public T Engine;
     }
+
+    class ElectricEngine { }
+
+    class GasEngine { }
 }
