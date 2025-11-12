860bfbf36e102226fe85116470cd487c868b68bb
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-11-12 18:42:45 +0700

    8.2.1

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -6,7 +6,23 @@ namespace CSharpLearn
     {
         public static void Main(string[] args)
         {
-            //задание 8.1.5
+            //задание 8.2.1
+
+            string dirName = @"C:\";
+
+            if (Directory.Exists(dirName))
+            {
+                int filesCount = Directory.GetFiles(dirName).Length;
+                Console.WriteLine($"Количество файлов в \"{dirName}\": {filesCount}");
+
+                int directoriesCount = Directory.GetDirectories(dirName).Length;
+                Console.WriteLine($"Количество файлов в \"{dirName}\": {directoriesCount}");
+            }
+
+            else
+            {
+                Console.WriteLine("Директории не существует");
+            }
         }
     }
 }
