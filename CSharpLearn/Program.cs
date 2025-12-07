340448e493839a5fb6792e1987830fa0b0a67854
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2025-12-08 01:51:56 +0700

    9.1.3

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace CSharpLearn
 {
@@ -7,27 +6,10 @@ namespace CSharpLearn
     {
         public static void Main(string[] args)
         {
-            //задание 8.4.3
+            //задание 9.1.3
 
-            Contact contact = new Contact("John Doe", 1234567890, "john.doe@example.com");
-
-            // Сериализация объекта в бинарный файл
-            using (FileStream fileStream = new FileStream("contact.bin", FileMode.Create))
-            using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
-            {
-                contact.Serialize(binaryWriter);
-            }
-
-            // Десериализация объекта из бинарного файла
-            Contact deserializedContact;
-            using (FileStream fileStream = new FileStream("contact.bin", FileMode.Open))
-            using (BinaryReader binaryReader = new BinaryReader(fileStream))
-            {
-                deserializedContact = Contact.Deserialize(binaryReader);
-            }
-
-            // Вывод десериализованных данных
-            Console.WriteLine($"Name: {deserializedContact.Name}, Phone: {deserializedContact.PhoneNumber}, Email: {deserializedContact.Email}");
+            Exception exception = new Exception();
+            exception.Data.Add("Дата создания исключения: ", DateTime.Now);
         }
     }
 }
