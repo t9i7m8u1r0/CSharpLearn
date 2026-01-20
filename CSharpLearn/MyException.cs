82202338f025c44c99324da8fd1262ef8889f9b8
t9i7m8u1r0 <t9i7m8u1r0@gmail.com>
2026-01-21 04:04:50 +0700

    9.6.1 (HW-03) - Task 1

---
+++ CSharpLearn/MyException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CSharpLearn
+{
+    class MyException(string message) : Exception(message)
+    {
+
+    }
+}
--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -2,21 +2,36 @@
 
 namespace CSharpLearn
 {
-    class Parent { }
-
-    class Child : Parent { }
-
     class MainClass
     {
-        delegate void ChildDelegate(Child child);
-
-        static void ParentMethod(Parent parent) { }
-
         static void Main(string[] args)
         {
-            //9.4.3
+            //9.6.1 (HW-03) - Task 1
 
-            ChildDelegate c = ParentMethod;
+            MyException[] myExceptions = new MyException[5]
+            {
+                new MyException("MyException1"),
+                new MyException("MyException2"),
+                new MyException("MyException3"),
+                new MyException("MyException4"),
+                new MyException("MyException5")
+            };
+
+            for (int i = 0; i < myExceptions.Length; i++)
+            {
+                try
+                {
+                    throw myExceptions[i];
+                }
+                catch (MyException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
